5b9fae86333711a2585bf13bbcca54987ae0edf7
Peter Michaux <petermichaux@gmail.com>
2012-04-22 20:45:16 -0700

    add tests for Leaf

--- tst/leafSuite.js
+++ tst/leafSuite.js
@@ -2,8 +2,23 @@
 
     buster.testCase('leafSuite', {
 
-        "test trivial": function() {
-            assert.same(0, 0);
+        "test leaf starts with null relationships": function() {
+            var leaf = new hijos.Leaf();
+            assert.same(null, leaf.parentNode);
+            assert.same(null, leaf.previousSibling);
+            assert.same(null, leaf.nextSibling);
+        },
+
+        "test leaf has a destroy method": function() {
+            // all nodes and leaves need a destroy function
+            var leaf = new hijos.Leaf();
+            assert.same('function', typeof leaf.destroy);
+        },
+
+        "test mixinLeaf adds destroy method": function() {
+            var obj = {};
+            hijos.mixinLeaf(obj);
+            assert.same('function', typeof obj.destroy);
         }
 
     });
