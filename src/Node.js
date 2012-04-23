daa3a49ec495b3c43de7e21a802d4b229dee9bed
Peter Michaux <petermichaux@gmail.com>
2012-04-22 20:28:45 -0700

    Simplify code of appendChild and replaceChild to reuse insertBefore and removeChild so that only two methods need to be overridden in subclasses (template pattern.) Also docs.

--- src/Node.js
+++ src/Node.js
@@ -30,20 +30,21 @@
 
 @description
 
-A constructor function for creating node objects with ordered children
+A constructor function for creating Node objects with ordered children
 to be used as part of the composite design pattern.
 
-Node objects have three read-only properties describing the node's
-relationships to other objects participating in the composite pattern.
+Node objects have three read-only properties describing the Node's
+relationships to other Leaf and Node objects participating in
+the composite pattern.
 
     1. childNodes
     2. firstChild
     3. lastChild
 
-The firstChild and lastChild properties will be undefined or null when
-the node has no children. Do not mutate the elements of the childNodes
-array directly. Instead use the appendChild, insertBefore, replaceChild,
-and removeChild methods to manage the children.
+The firstChild and lastChild properties will be null when the Node has
+no children. Do not mutate the elements of the childNodes array directly.
+Instead use the appendChild, insertBefore, replaceChild, and removeChild
+methods to manage the children.
 
 var node = new hijos.Node();
 
@@ -51,6 +52,8 @@ var node = new hijos.Node();
 hijos.Node = function() {
     hijos.Leaf.call(this);
     this.childNodes = [];
+    this.firstChild = null;
+    this.lastChild = null;
 };
 
 hijos.mixinLeaf(hijos.Node.prototype);
@@ -63,18 +66,20 @@ hijos.mixinLeaf(hijos.Node.prototype);
 
 Call before your application code looses its last reference to the object.
 Generally this will be called for you by the destroy method of the containing
-node object unless this object is not contained by another node.
+Node object unless this object is not contained by another Node.
 
 */
 hijos.Node.prototype.destroy = function() {
-    if (Object.prototype.hasOwnProperty.call(this, 'childNodes')) {
-        // copy in case one of the destroy methods modifies this.childNodes
-        var children = this.childNodes.slice(0);
-        for (var i = 0, ilen = children.length; i < ilen; i++) {
-            children[i].destroy();
-        }
+    // copy in case one of the destroy methods modifies this.childNodes
+    var children = this.childNodes.slice(0);
+    for (var i = 0, ilen = children.length; i < ilen; i++) {
+        children[i].destroy();
     }
     hijos.Leaf.prototype.destroy.call(this);
+    // Loosing references to relations may help garbage collection.
+    this.childNodes = null;
+    this.firstChild = null;
+    this.lastChild = null;
 };
 
 /**
@@ -83,87 +88,28 @@ hijos.Node.prototype.destroy = function() {
 
 @description
 
-Returns true if this node has children. Otherwise returns false.
+Returns true if this Node has children. Otherwise returns false.
 
 */
 hijos.Node.prototype.hasChildNodes = function() {
-    return Object.prototype.hasOwnProperty.call(this, 'childNodes') &&
-           (this.childNodes.length > 0);
+    return this.childNodes.length > 0;
 };
 
 (function() {
-
-    function wouldBeCircular(node, possibleAncestor) {
-        while (node) {
-            if (node === possibleAncestor) {
-                return true;
-            }
-            node = node.parentNode;
-        }
-        return false;
-    };
-
-/**
-
-@property hijos.Node.prototype.appendChild
-
-@parameter newChild {object} The leaf or node object to append.
-
-@description
-
-Adds newChild as the last child of this node. If newChild is a child of
-another node then newChild is removed from that other node before appending
-to this node.
-
-var parent = new hijos.Node();
-var child = new hijos.Leaf();
-parent.appendChild(child);
-var child = new hijos.Node();
-parent.appendChild(child);
-
-*/
-    hijos.Node.prototype.appendChild = function(newChild) {
-        if (arguments.length < 1) {
-            throw new Error('hijos.Node.prototype.appendChild: not enough arguments.');
-        }
-        // do not allow the creation of a circular tree structure
-        if (wouldBeCircular(this, newChild)) {
-            throw new Error('hijos.Node.prototype.appendChild: Node cannot be inserted at the specified point in the hierarchy.');
-        }
-        // remove from previous composite
-        var parent = newChild.parentNode;
-        if (parent) {
-            parent.removeChild(newChild);
-        }
-        // add to this composite
-        if (!Object.prototype.hasOwnProperty.call(this, 'childNodes')) {
-            this.childNodes = [];
-        }
-        var children = this.childNodes;
-        children.push(newChild);
-        this.firstChild = children[0];
-        this.lastChild = newChild;
-        newChild.parentNode = this;
-        var previousSibling = newChild.previousSibling = children[children.length - 2];
-        if (previousSibling) {
-            previousSibling.nextSibling = newChild;
-        }
-        newChild.nextSibling = null;
-    };
 
 /**
 
 @property hijos.Node.prototype.insertBefore
 
-@parameter newChild {object} The leaf or node object to insert.
+@parameter newChild {object} The Leaf or Node object to insert.
 
 @parameter oldChild {object|null} The child object to insert before.
 
 @description
 
 Inserts newChild before oldChild. If oldChild is null then this is equivalent
-to appending newChild. If newChild is a child of another node then newChild is
-removed from that other node before appending to this node.
+to appending newChild. If newChild is a child of another Node then newChild is
+removed from that other Node before appending to this Node.
 
 var parent = new hijos.Node();
 var child0 = new hijos.Leaf();
@@ -176,33 +122,42 @@ parent.insertBefore(child1, child0);
         if (arguments.length < 2) {
             throw new Error('hijos.Node.prototype.insertBefore: not enough arguments.');
         }
-        // newChild is already in correct position
-        if (newChild === oldChild) {
-            return;
+        // Allow caller to be sloppy and send undefined instead of null.
+        if (oldChild === undefined) {
+            oldChild = null;
         }
-        // if oldChild is null then want to append to end of children
-        if ((oldChild === null) || (oldChild === undefined)) {
-            this.appendChild(newChild);
+        // Is newChild is already in correct position?
+        if ((newChild === oldChild) || // inserting a node before itself
+            (oldChild && (oldChild.previousSibling === newChild)) || // inserting newChild where it already is
+            ((oldChild === null) && this.lastChild === newChild)) { // inserting child at end when it is already at the end
             return;
         }
         // do not allow the creation of a circular tree structure
-        if (wouldBeCircular(this, newChild)) {
-            throw new Error('hijos.Node.prototype.insertBefore: Node cannot be inserted at the specified point in the hierarchy.');
-        }
-        // continue with normal insertion
-        if (!Object.prototype.hasOwnProperty.call(this, 'childNodes')) {
-            this.childNodes = [];
-        }
-        var children = this.childNodes;
-        // find index of oldChild
-        for (var i = 0, ilen = children.length; i < ilen; i++) {
-            if (children[i] === oldChild) {
-                var indexOfOldChild = i;
-                break;
+        var node = this;
+        while (node) {
+            if (node === newChild) {
+                throw new Error('hijos.Node.prototype.insertBefore: Node cannot be inserted at the specified point in the hierarchy.');
             }
+            node = node.parentNode;
         }
-        if (typeof indexOfOldChild !== 'number') {
-            throw new Error('hijos.Node.prototype.insertBefore: Node was not found.');
+        // continue with insertion
+        var children = this.childNodes;
+        // find index for newChild
+        var indexForNewChild;
+        if (oldChild === null) {
+            // want to append to end of children
+            indexForNewChild = children.length;
+        }
+        else {
+            for (var i = 0, ilen = children.length; i < ilen; i++) {
+                if (children[i] === oldChild) {
+                    indexForNewChild = i;
+                    break;
+                }
+            }
+            if (typeof indexForNewChild !== 'number') {
+                throw new Error('hijos.Node.prototype.insertBefore: Node was not found.');
+            }
         }
         // remove from previous composite
         var parent = newChild.parentNode;
@@ -226,16 +181,42 @@ parent.insertBefore(child1, child0);
 
 /**
 
-@property hijos.Node.prototype.replaceChild
+@property hijos.Node.prototype.appendChild
 
-@parameter newChild {object} The leaf or node object to insert.
-
-@parameter oldChild {object} The child object to remove and replace.
+@parameter newChild {object} The Leaf or Node object to append.
 
 @description
 
-Replaces oldChild with newChild. If newChild is a child of another node
-then newChild is removed from that other node before appending to this node.
+Adds newChild as the last child of this Node. If newChild is a child of
+another Node then newChild is removed from that other Node before appending
+to this Node.
+
+var parent = new hijos.Node();
+var child = new hijos.Leaf();
+parent.appendChild(child);
+var child = new hijos.Node();
+parent.appendChild(child);
+
+*/
+    hijos.Node.prototype.appendChild = function(newChild) {
+        if (arguments.length < 1) {
+            throw new Error('hijos.Node.prototype.appendChild: not enough arguments.');
+        }
+        this.insertBefore(newChild, null);
+    };
+
+/**
+
+@property hijos.Node.prototype.replaceChild
+
+@parameter newChild {object} The Leaf or Node object to insert.
+
+@parameter oldChild {object} The child object to remove/replace.
+
+@description
+
+Replaces oldChild with newChild. If newChild is a child of another Node
+then newChild is removed from that other Node before appending to this Node.
 
 var parent = new hijos.Node();
 var child0 = new hijos.Leaf();
@@ -251,44 +232,13 @@ parent.replaceChild(child1, child0);
         if (!oldChild) {
             throw new Error('hijos.Node.prototype.replaceChild: oldChild must not be falsy.');
         }
-        // child is already in correct position
+        // child is already in correct position and
+        // do not want removeChild to be called below
         if (newChild === oldChild) {
             return;
         }
-        // do not allow the creation of a circular tree structure
-        if (wouldBeCircular(this, newChild)) {
-            throw new Error('hijos.Node.prototype.replaceChild: Node cannot be inserted at the specified point in the hierarchy.');
-        }
-        // continue with normal insertion
-        if (!Object.prototype.hasOwnProperty.call(this, 'childNodes')) {
-            this.childNodes = [];
-        }
-        var children = this.childNodes;
-        // find index of oldChild
-        for (var i = 0, ilen = children.length; i < ilen; i++) {
-            if (children[i] === oldChild) {
-                var indexOfOldChild = i;
-                break;
-            }
-        }
-        if (typeof indexOfOldChild !== 'number') {
-            throw new Error('hijos.Node.prototype.replaceChild: Node was not found.');
-        }
-        children[indexOfOldChild] = newChild;
-        this.firstChild = children[0];
-        this.lastChild = children[children.length - 1];
-        newChild.parentNode = this;
-        oldChild.parentNode = null;
-        var previousSibling = newChild.previousSibling = oldChild.previousSibling;
-        if (previousSibling) {
-            previousSibling.nextSibling = newChild;
-        }
-        oldChild.previousSibling = null;
-        var nextSibling = newChild.nextSibling = oldChild.nextSibling;
-        if (nextSibling) {
-            nextSibling.previousSibling = newChild;
-        }
-        oldChild.previousSibling = null;
+        this.insertBefore(newChild, oldChild);
+        this.removeChild(oldChild);
     };
 
 }());
@@ -313,26 +263,24 @@ hijos.Node.prototype.removeChild = function(oldChild) {
     if (arguments.length < 1) {
         throw new Error('hijos.Node.prototype.removeChild: not enough arguments.');
     }
-    if (Object.prototype.hasOwnProperty.call(this, 'childNodes')) {
-        var children = this.childNodes;
-        for (var i = 0, ilen = children.length; i < ilen; i++) {
-            if (children[i] === oldChild) {
-                var previousSibling = children[i - 1];
-                if (previousSibling) {
-                    previousSibling.nextSibling = oldChild.nextSibling;
-                }
-                var nextSibling = children[i + 1];
-                if (nextSibling) {
-                    nextSibling.previousSibling = oldChild.previousSibling;
-                }
-                oldChild.parentNode = null;
-                oldChild.previousSibling = null;
-                oldChild.nextSibling = null;
-                children.splice(i, 1);
-                this.firstChild = children[0];
-                this.lastChild = children[children.length - 1];
-                return; // stop looking
+    var children = this.childNodes;
+    for (var i = 0, ilen = children.length; i < ilen; i++) {
+        if (children[i] === oldChild) {
+            var previousSibling = children[i - 1];
+            if (previousSibling) {
+                previousSibling.nextSibling = oldChild.nextSibling;
             }
+            var nextSibling = children[i + 1];
+            if (nextSibling) {
+                nextSibling.previousSibling = oldChild.previousSibling;
+            }
+            oldChild.parentNode = null;
+            oldChild.previousSibling = null;
+            oldChild.nextSibling = null;
+            children.splice(i, 1);
+            this.firstChild = children[0];
+            this.lastChild = children[children.length - 1];
+            return; // stop looking
         }
     }
     throw new Error('hijos.Node.prototype.removeChild: node not found.');
@@ -342,12 +290,12 @@ hijos.Node.prototype.removeChild = function(oldChild) {
 
 @property hijos.mixinNode
 
-@parameter obj {object} The object to become a node.
+@parameter obj {object} The object to become a Node.
 
 @description
 
-Mixes in the node methods into any object. Be sure to call the hijos.Node
-constructor to initialize the node's properties.
+Mixes in the Node methods into any object. Be sure to call the hijos.Node
+constructor to initialize the Node's properties.
 
 app.MyView = function() {
     hijos.Node.call(this);
