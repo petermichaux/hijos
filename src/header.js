3dbf92b3fc8e36eca283d25bc414e48c24ab1cd5
Peter Michaux <petermichaux@gmail.com>
2012-04-27 12:26:40 -0700

    header file for build process

---
+++ src/header.js
@@ -0,0 +1,7 @@
+/*
+Hijos version 0 - JavaScript classes for building tree structures and the composite design pattern
+Copyright (c) 2012, Peter Michaux
+All rights reserved.
+Licensed under the Simplified BSD License.
+https://github.com/petermichaux/hijos/blob/master/LICENSE
+*/
