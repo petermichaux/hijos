b62854aed4d8a4996ec1a517de730bca48641b11
Peter Michaux <petermichaux@gmail.com>
2013-04-08 09:01:56 -0700

    update version

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
 /*
-Hijos version 2
-Copyright (c) 2012, Peter Michaux
+Hijos version 3
+Copyright (c) 2013, Peter Michaux
 All rights reserved.
 Licensed under the Simplified BSD License.
 https://github.com/petermichaux/hijos/blob/master/LICENSE
